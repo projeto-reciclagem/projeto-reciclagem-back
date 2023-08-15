00258a91abc7823a3993e320ab6625c34af444c6
Nilton-Izio <nilton.gomes@izio.com.br>
2023-08-15 14:17:49 -0300

    ADD: Estrutura de pasta

---
+++ src/main/java/com/projeto/sprint/projetosprint/ProjetoSprintApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.sprint.projetosprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/sprint/projetosprint/ProjetoSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.sprint.projetosprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
