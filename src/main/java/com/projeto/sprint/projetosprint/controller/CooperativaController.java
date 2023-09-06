82ff6bb70c450dad994d57c0687284102cac4143
Nilton-SPTech <nilton.gomes@sptech.school>
2023-09-06 08:25:49 -0300

    UPDATE: Adicionando as controllers que de put e delete e atualizando o cadastro da Cooperativa

--- src/main/java/com/projeto/sprint/projetosprint/controller/CooperativaController.java
+++ src/main/java/com/projeto/sprint/projetosprint/controller/CooperativaController.java
@@ -19,10 +19,24 @@ public class CooperativaController {
     @PostMapping("/cadastrarCooperativa")
     public ResponseEntity<Cooperativa> cadastrarCooperativa(@RequestBody Cooperativa dados){
 
+        //VALIDAÇÕES DOS CAMPOS QUE SERÃO RECEBIDOS
         if(dados.getNome() == null || dados.getNome().isBlank()){
             return ResponseEntity.status(400).build();
         }
 
+        if(dados.getCnpj().isBlank() || dados.getCnpj().isEmpty()){
+            return ResponseEntity.status(400).build();
+        }
+
+        if(dados.getEmail().isEmpty() || dados.getEmail().isBlank()){
+            return ResponseEntity.status(400).build();
+        }
+
+        if(dados.getSenha().isEmpty() || dados.getSenha().isBlank()){
+            return ResponseEntity.status(400).build();
+        }
+
+        //ADICIONANDO UMA NOVA COOPERATIVA
         listCooperativa.add(dados);
         return ResponseEntity.status(201).body(dados);
     }
@@ -36,8 +50,42 @@ public class CooperativaController {
         return ResponseEntity.status(200).body(listCooperativa);
     }
 
-    @PutMapping("/atualizarCooperativa")
+    @PutMapping("/atualizarCooperativaPorId")
     public ResponseEntity<Cooperativa> atualizarCooperativa(@RequestParam int indice, @RequestBody Cooperativa dados){
 
+        //VALIDAÇÕES DOS CAMPOS QUE SERÃO RECEBIDOS
+        if(indice < 0 || indice >= listCooperativa.size()){
+            return ResponseEntity.status(400).build();
+        }
+        if(dados.getNome() == null || dados.getNome().isBlank()){
+            return ResponseEntity.status(400).build();
+        }
+
+        if(dados.getCnpj().isBlank() || dados.getCnpj().isEmpty()){
+            return ResponseEntity.status(400).build();
+        }
+
+        if(dados.getEmail().isEmpty() || dados.getEmail().isBlank()){
+            return ResponseEntity.status(400).build();
+        }
+
+        if(dados.getSenha().isEmpty() || dados.getSenha().isBlank()){
+            return ResponseEntity.status(400).build();
+        }
+
+        //ATUALIZANDO A COOPERATIVA
+        listCooperativa.set(indice, dados);
+        return ResponseEntity.status(200).body(dados);
     }
+
+    @DeleteMapping("/deletarCooperativaPorId")
+    public ResponseEntity<Void> deletarCooperativaPorId(@RequestParam int indice){
+        if(indice < 0 || indice >= listCooperativa.size()){
+            return ResponseEntity.status(200).build();
+        }
+
+        listCooperativa.remove(indice);
+        return ResponseEntity.status(200).build();
+    }
+
 }
