8764304b531182465a3a48b94adbaf2fb28940fb
Nilton-Gabriel <niltongabrielramos@gmail.com>
2023-10-15 22:25:43 -0300

    ADD: Adicionando endpoint gravar arquivo CSV

--- src/main/java/com/projeto/sprint/projetosprint/api/controller/CooperativaController.java
+++ src/main/java/com/projeto/sprint/projetosprint/api/controller/CooperativaController.java
@@ -2,12 +2,18 @@ package com.projeto.sprint.projetosprint.api.controller;
 
 import com.projeto.sprint.projetosprint.domain.cooperativa.Cooperativa;
 import com.projeto.sprint.projetosprint.service.cooperativa.CooperativaService;
+import com.projeto.sprint.projetosprint.util.ListaObj;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URI;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.*;
 
 
 @RestController
@@ -66,6 +72,124 @@ public class CooperativaController {
 
         this.service.deletarCooperativa(id);
         return ResponseEntity.noContent().build();
+    }
+
+    //GRAVANDO OS DADOS EM UM ARQUIVO CSV
+    @PostMapping("/gravarArquivoCsv")
+    public ResponseEntity<Void> gravaArquivoCsv(@RequestParam String nomeArq){
+        FileWriter arq = null; //REPRESENTA O ARQUIVO QUE SERÁ GRAVADO
+        Formatter saida = null; //SERÁ USADO PARA ESCREVER NO ARQUIVO
+        Boolean deuRuim = false; //VALIDA SE ALGO NÃO DEU CERTO
+
+
+        List<Cooperativa> lista = this.service.listarCooperativa();
+
+        String dateStamp = LocalDateTime.now()
+                .toString()
+                .replace(":", "-")
+                .replace(".", "-");
+
+        nomeArq +=  "-" + dateStamp + ".csv"; //ACRESCENTA A EXTENSÃO
+
+        //CRIANDO O ARQUIVO || ABRINDO O ARQUIVO
+        try {
+            arq = new FileWriter(nomeArq); //ABRE O ARQUIVO
+            saida = new Formatter(arq); //INSTANCIA O OBJ SAIDA, ASSOCIANDO COM O ARQ
+        }
+        catch (IOException e){
+            System.out.println("Erro ao abrir o arquivo");
+            System.exit(1);
+        }
+
+        // GRAVANDO O ARQUIVO
+        try{
+            for (Cooperativa c : lista){
+                //GRAVANDO OS DADOS DA COOPERATIVA NO ARQUIVO
+                saida.format("%d;%s;%s;%s\n",
+                        c.getId(), c.getNome(),c.getCnpj(),c.getEmail());
+            }
+
+        }
+        catch (FormatterClosedException err){
+            System.out.println("Erro ao gravar o arquivo");
+            deuRuim = false;
+        }
+        finally {
+            saida.close();
+
+            try{
+                arq.close();
+            }
+            catch (IOException err){
+                System.out.println("Erro ao fechar o arquivo");
+                deuRuim = false;
+            }
+
+            if (deuRuim){
+                System.exit(1);
+            }
+        }
+
+        return ResponseEntity.noContent().build();
+    }
+
+
+    public static void leExibeArquivoCsv(String nomeAr) {
+        FileReader arq = null; //REPRESENTA O ARQUIVO A SER LIDO
+        Scanner entrada = null;
+        boolean deuRuim = false;
+
+        //ACRESCENTA A EXTENÇÃO .csv
+        nomeAr += ".csv";
+
+        //ABRINDO O ARQUIVO
+        try {
+            arq = new FileReader(nomeAr);
+            entrada = new Scanner(arq).useDelimiter(";|\\n");
+        }
+        catch (FileNotFoundException error){
+            System.out.println("Arquivo inexistente");
+            System.exit(1);
+        }
+
+        //LENDO CADA LINHA DO ARQUIVO
+        try {
+            System.out.printf("%-4S %-15S %-9S %4S\n", "id", "nome", "porte", "peso");
+
+            //hasNext() RETORNA TRUE ENQUANTO TEM LINHAS PARA LER NO ARQUIVO
+            while (entrada.hasNext()){
+                int id = entrada.nextInt();
+                String nome = entrada.next();
+                String porte = entrada.next();
+                Double peso = entrada.nextDouble();
+
+                System.out.printf("%04d %-15s %-9s %4.1f\n", id, nome, porte, peso);
+            }
+        }
+        catch (NoSuchElementException error){
+            System.out.println("Arquivo com problemas");
+            error.printStackTrace();
+        }
+        catch (IllegalStateException error){
+            System.out.println("Erro na leitura do arquivo");
+            error.printStackTrace();
+            deuRuim = true;
+        }
+        finally {
+            entrada.close();
+
+            try{
+                arq.close();
+            }
+            catch (IOException err){
+                System.out.println("Erro ao fechar o arquivo");
+                deuRuim = false;
+            }
+
+            if (deuRuim){
+                System.exit(1);
+            }
+        }
     }
 
 }
---
+++ src/main/java/com/projeto/sprint/projetosprint/util/ListaObj.java
@@ -0,0 +1,75 @@
+package com.projeto.sprint.projetosprint.util;
+
+public class ListaObj <T>{
+
+
+    private Object[] vetor;
+    private int nroElem;
+
+    public ListaObj(int tamanhoLista){
+        vetor = (T[]) new Object[tamanhoLista];
+        nroElem = 0;
+    }
+
+    public void adiciona(T valor){
+        vetor[nroElem] = valor;
+        nroElem++;
+
+    }
+
+    public void exibe(){
+        if(nroElem == 0){
+            System.out.println("\nLista está vazia");
+        }
+        else{
+            for (int i = 0; i < nroElem; i++){
+                System.out.println(vetor[i]);
+            }
+        }
+    }
+
+    public int busca(T elemento){
+        for (int i = 0; i < nroElem; i++){
+            if (vetor[i].equals(elemento)){
+                return i;
+            }
+        }
+        return  -1;
+    }
+
+    public boolean removerPeloIndice(int indice){
+        if(indice < 0 || indice > nroElem){
+            return  false;
+        }
+        nroElem--;
+
+        for (int i = indice; i < nroElem; i++){
+            vetor[i] = vetor[i + 1];
+        }
+
+        return true;
+    }
+
+    public boolean removeElemento(T elemento){
+        return removerPeloIndice(busca(elemento));
+    }
+
+    public int getTamanho(){return nroElem;}
+
+    public T getElemento(int indice){
+        if(indice >= 0 || indice < vetor.length){
+            return (T) vetor[indice];
+        }
+        System.out.println("Indice Inválido");
+        return null;
+    }
+
+    public void limpa(){
+        for (Object item : vetor){
+            item = null;
+        }
+        nroElem = 0;
+        System.out.println("O vetor está limpo");
+    }
+
+}
