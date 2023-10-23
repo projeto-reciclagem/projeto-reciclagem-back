9e280e4d17d83befecd5d695665920850b7b5a6a
Nilton-Gabriel <niltongabrielramos@gmail.com>
2023-10-23 17:34:46 -0300

    ADD: Adicionando padrão de projeto observer enviando email

---
+++ src/main/java/com/projeto/sprint/projetosprint/domain/email/EmailBoasVindas.java
@@ -0,0 +1,45 @@
+package com.projeto.sprint.projetosprint.domain.email;
+
+import com.projeto.sprint.projetosprint.domain.email.observer.Destinatario;
+import com.projeto.sprint.projetosprint.service.email.EmailService;
+import java.util.UUID;
+
+public class EmailBoasVindas implements Destinatario {
+
+    private UUID id = UUID.randomUUID();
+
+    private String nome;
+    private String email;
+    private static final String EMAIL_EMPRESA = "niltongabrielramos@gmail.com";
+
+    public EmailBoasVindas(){}
+    public EmailBoasVindas(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    @Override
+    public void enviarEmail(EmailService emailService, EmailConteudo emailConteudo) {
+        emailService.sendEmail(EMAIL_EMPRESA, this.email, emailConteudo.getTitulo(), emailConteudo.getConteudo());
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/projeto/sprint/projetosprint/service/email/EmailConteudoService.java
@@ -0,0 +1,49 @@
+package com.projeto.sprint.projetosprint.service.email;
+
+import com.projeto.sprint.projetosprint.domain.email.EmailConteudo;
+import com.projeto.sprint.projetosprint.domain.email.observer.Destinatario;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmailConteudoService {
+
+    private EmailService service;
+
+    public EmailConteudoService(EmailService service) {
+        this.service = service;
+    }
+
+    private List<EmailConteudo> emailConteudos = new ArrayList<>();
+
+    public void publicarEmail(UUID idEmail){
+        EmailConteudo emailConteudo = this.buscarPorIndice(idEmail);
+        emailConteudo.getEmailList()
+                .forEach(e -> e.enviarEmail(service, emailConteudo));
+    }
+
+    public void adicionarDestinatario(UUID idEmail, Destinatario destinatario){
+        EmailConteudo email = this.buscarPorIndice(idEmail);
+        email.inscrever(destinatario);
+    }
+
+    public UUID criarEmail(EmailConteudo emailConteudo){
+        this.emailConteudos.add(emailConteudo);
+        return emailConteudo.getId();
+    }
+
+    public List<EmailConteudo> listarEmail(){return this.emailConteudos;}
+
+    public EmailConteudo buscarPorId(UUID id){return this.buscarPorIndice(id);}
+    private EmailConteudo buscarPorIndice(UUID id) {
+        return this.emailConteudos.stream()
+                .filter(e -> e.getId().equals(id))
+                .findFirst()
+                .orElseThrow(
+                        () -> new IllegalArgumentException("Email não encontrado!")
+                );
+    }
+}
