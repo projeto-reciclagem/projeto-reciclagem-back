f602788747a9263ba48b70329b0431d4679d46ce
Nilton-Gabriel <niltongabrielramos@gmail.com>
2023-11-23 17:44:47 -0300

    UPDATE: Arrumando o erro de service

--- src/main/java/com/projeto/sprint/projetosprint/domain/repository/CondominioRepository.java
+++ src/main/java/com/projeto/sprint/projetosprint/domain/repository/CondominioRepository.java
@@ -4,5 +4,4 @@ import com.projeto.sprint.projetosprint.domain.entity.condominio.Condominio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CondominioRepository extends JpaRepository<Condominio, Integer> {
-    Boolean existsByEmail(String email);
 }
