510fbd2094c5b94fcfd98ed719957b0ab9b5745f
Marcus Vinicius Gomes Gonçalves <marcusvggoncalves@gmail.com>
2024-06-17 04:30:44 -0300

    fix: @Service missing in TwoFactorAuthService

--- src/main/java/com/projeto/sprint/projetosprint/service/twofactorauth/TwoFactorAuthService.java
+++ src/main/java/com/projeto/sprint/projetosprint/service/twofactorauth/TwoFactorAuthService.java
@@ -3,9 +3,11 @@ package com.projeto.sprint.projetosprint.service.twofactorauth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
 
+@Service
 public class TwoFactorAuthService {
 
     @Autowired
